32dc91df7cdb47da2f3b78e4e5464670e1dd9201
bimal-tech <bimaladhikari8158@gmail.com>
2023-06-12 17:19:33 +0545

    new custom canvas

--- customChart/noLineCanvas.js
+++ customChart/noLineCanvas.js
@@ -6,13 +6,13 @@ function loadcanvas(canvas_id, canvas_width, atl, cp, ath) {
     var line_height = 10;
     ctx.font = "10pt Times New Roman";
 
-    var line_type_1_params = [0.15, 0.67, 0.8];
-    var line_type_2_params = [0.06, 0.8];
-    var line_type_3_params = [0.15, 0.47, 0.67, 0.6, 0.65];
+    var line_type_1_params = [0.22, 0.65, 0.8];
+    var line_type_3_params = [0.22, 0.47, 0.64, 0.6, 0.65];
 
-    // line1(ctx, 35, canvas_width, line_height, atl, cp, ath, line_type_1_params);
+    line1(ctx, 35, canvas_width, line_height, atl, cp, ath, line_type_1_params);
     line2(ctx, 60, canvas_width, line_height, atl, ath, cp);
-    // line3(ctx, 85, canvas_width, line_height, atl, cp, ath, line_type_3_params);
+    line3(ctx, 85, canvas_width, line_height, atl, cp, ath, line_type_3_params);
+    current_price_circle(ctx, 60, canvas_width, line_height, atl, cp, ath);
 }
 
 function line1(ctx, height, width, line_height, atl, cp, ath, line_params) {
@@ -21,10 +21,7 @@ function line1(ctx, height, width, line_height, atl, cp, ath, line_params) {
 
     ctx.fillText(diff_cp_ath(cp, ath), (width * line_params[1]), (height + line_height + 7));
 
-
     ctx.fillText(diff_atl_ath(atl, ath), (width * line_params[2]), (height + line_height * 0.8));
-
-
 }
 
 function line2(ctx, height, width, line_height, atl, ath, cp) {
@@ -33,9 +30,7 @@ function line2(ctx, height, width, line_height, atl, ath, cp) {
     ctx.fillText('$' + atl, 0, (height + line_height * 0.25));
     ctx.setLineDash([5, 0]);
     ctx.beginPath();
-
-
-    let atl_cp_line_difference = width * (0.97 - (atl.length - 1) / 100);
+    let atl_cp_line_difference = width * (0.94 - (atl.toString().length - 1) / 100);
     ctx.moveTo(width - atl_cp_line_difference, height);
     ctx.lineTo(width - width / 1.7, height);
     ctx.stroke();
@@ -44,13 +39,14 @@ function line2(ctx, height, width, line_height, atl, ath, cp) {
     ctx.beginPath();
 
     cp = Number(cp).toFixed(2);
-    let cp_ath_line_difference = width * (0.49 - (cp.length - 6) / 100);
+    let cp_ath_line_difference_starting = width * (0.47 - (cp.toString().length - 3) / 100);
+    ctx.moveTo(width - cp_ath_line_difference_starting, height);
 
-
-    ctx.moveTo(width - cp_ath_line_difference, height);
-    ctx.lineTo(width - width * 0.21, height);
+    ath = Number(ath).toFixed(2);
+    let cp_ath_line_difference_ending = width * (0.16 + (ath.toString().length - 4) / 100);
+    ctx.lineTo(width - cp_ath_line_difference_ending, height);
     ctx.stroke();
-    ctx.fillText('$' + Number(ath).toFixed(2), (width * 0.8), (height + line_height * 0.25));
+    ctx.fillText('$' + ath, (width - cp_ath_line_difference_ending + 4), (height + line_height * 0.25));
 
 }
 
@@ -67,6 +63,13 @@ function line3(ctx, height, width, line_height, atl, cp, ath, line_params) {
     ctx.fillText(diff_ath_cp(cp, ath), (width * line_params[2]), height - 7);
 }
 
+function current_price_circle(ctx, height, width, line_height, atl, cp, ath) {
+    ctx.beginPath();
+    ctx.arc(diff_for_circle(atl, cp, ath) * width, (height + line_height * 0.0), 4, 0, 2 * Math.PI);
+    ctx.fillStyle = 'black';
+    ctx.stroke();
+    ctx.fill();
+}
 
 function diff_atl_ath(atl, ath) {
     var perc = 0;
@@ -125,4 +128,28 @@ function diff_atl_ath_cp(atl, cp, ath) {
 
     let diff = (ath - atl) / 2;
     return cp > diff ? 1 : 0;
+}
+
+function diff_for_circle(atl, cp, ath) {
+    // ath = Number(ath).toFixed(2);
+
+    // if (cp == 0 || cp == atl) {
+    //     return 0.08;
+    // }
+    // if (cp == ath || ath - cp < 1) {
+    //     let cp_ath_line_difference_ending = (0.16 + (ath.toString().length - 4) / 100);
+    //     return 1 - cp_ath_line_difference_ending - 0.01;
+    // }
+
+    if (cp == 0) {
+        return 1.02 - (0.94 - (atl.toString().length - 1) / 100);
+    }
+    let diff = (ath - atl - cp + atl) / (ath - atl);
+    if (diff > 0.8) {
+        return 1 - (0.16 + (ath.toString().length - 2) / 100);
+    }
+    if (diff < 0.2) {
+        return (0.94 - (atl.toString().length - 1) / 100);
+    }
+    return diff;
 }
